21a75e0062d1a5d04c5ff4663c6fb07fa6cc4682
Jutri0 <juliand_triana@soy.sena.edu.co>
2023-09-19 19:12:49 -0500

    Update: Funciones y Aspecto
    1. Se modifica el modelo de Movimiento de acuerdo a los cabios en el Backend.
    
    2. Se actualiza el componente HTML de Movimiento
    
    3. Se agregan nuevos botones en Movimientos para acceder a otras vistas.
    
    4. Se cambia el aspecto de las tabla usuarios, estado, rol.

--- Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/Modelos/Movimiento.model.ts
+++ Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/Modelos/Movimiento.model.ts
@@ -1,13 +1,13 @@
-export class MovimientoModel{
+export class MovimientoModel {
     constructor(
-        public IdMovimiento:string,
-        public FechaMovimiento:string,
-        public CantidadProducto:string,
-        public PrecioProductoMovimiento:string,
-        public IdMotivo:string,
-        public IdUbicacionAlmacen:string,
-        public NITProveedor:string,
-        public IdProductoMateriaPrima:string,
-        public IdUsuario:string
-    ){}
+        public IdMovimiento: string,
+        public FechaMovimiento: string,
+        public CantidadProducto: string,
+        public PrecioProductoMovimiento: string,
+        public DescripcionMovimiento: string, // Agregar descripción de motivo
+        public NombreAlmacen: string, // Agregar nombre de almacén
+        public NombreProveedor: string, // Agregar nombre de proveedor
+        public NombreProducto: string, // Agregar nombre de producto materia prima
+        public NombreUsuario: string // Agregar nombre de usuario
+    ) {}
 }
