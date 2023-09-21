9d70013561f26d44b296e52e69bf0a3ddaef4239
jcmican1 <119264781+jcmican1@users.noreply.github.com>
2023-09-21 17:10:52 -0500

    UPDATE: navegacion

--- Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/servicios/Compartidos/compartidos.service.ts
+++ Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/servicios/Compartidos/compartidos.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CompartidosService {
-  Sesion: boolean = false;
+  Sesion: boolean = true;
 
   constructor() { }
 }
