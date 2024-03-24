da2fda68956b27d7dee22e6357a9bd1ef07298fd
Alex <you@example.com>
2024-03-24 00:27:53 -0500

    UPDATE: Graficos reportescomponent.ts

--- Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/Componentes/TablasVista/reportes/reportes.component.ts
+++ Documentacion/Desarrollo/ProductoFuncional/ProyectoAngularSIMP/SIMP/src/app/Componentes/TablasVista/reportes/reportes.component.ts
@@ -1,22 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ReportesModel } from 'src/app/Modelos/Reportes.model';
 import { ReportesService } from 'src/app/servicios/reportes/reportes.service';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css']
 })
-export class ReportesComponent {
-  ReportesModel: Observable<ReportesModel[]> | undefined
+export class ReportesComponent implements OnInit {
+  reportes: ReportesModel[] = [];
 
   constructor(
     private ReportesService: ReportesService) { }
 
 
-  ngOnInit() {
-    this.ReportesModel = this.ReportesService.obtenerReportes();
+  ngOnInit(): void {
+    this.fetchReportes();
 
+  }
+  fetchReportes(): void {
+    this.ReportesService.obtenerReportes().subscribe(
+      (data: ReportesModel[]) => {
+        this.reportes = data;
+        this.renderBarChart();
+      },
+    );
+  }
+
+  renderBarChart(): void {
+    const canvas: any= document.getElementById('barChart');
+    const ctx = canvas.getContext('2d');
+
+    const nombresProductos = this.reportes.map(reporte => reporte.NombreProducto);
+    const totalVendido = this.reportes.map(reporte => parseFloat(reporte.TotalVendido));
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: nombresProductos,
+        datasets: [{
+          label: 'Total Vendido',
+          data: totalVendido,
+          backgroundColor: 'rgba(6, 23, 39)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
 }
