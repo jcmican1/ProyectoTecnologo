471fd930761de0850a496bd7f5e872e0ea579213
Alex <you@example.com>
2024-03-29 18:55:48 -0500

    UPDATE: Se agregaron dos nuevas rutas

--- Documentacion/Desarrollo/ProductoFuncional/BackendSIMP/routers/reportesRouter.js
+++ Documentacion/Desarrollo/ProductoFuncional/BackendSIMP/routers/reportesRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const conexion = require('../conexion');
 
-router.get('/', (req, res) => {
+router.get('/masvendido', (req, res) => {
     const query = `SELECT NombreProducto, SUM(CantidadProducto) AS TotalVendido
     FROM Movimiento
     JOIN Producto_Materia_Prima ON Movimiento.IdProductoMateriaPrima = Producto_Materia_Prima.IdProductoMateriaPrima
@@ -10,6 +10,24 @@ router.get('/', (req, res) => {
     GROUP BY NombreProducto
     ORDER BY TotalVendido DESC;`;
     conexion.query(query, (error, resultado) => {
+        if (error) return console.error(error.message);
+
+        if (resultado.length > 0) {
+            res.json(resultado);
+        } else {
+            res.json(`No hay registros en la tabla de reportes`)
+        }
+    });
+});
+
+router.get('/menosvendido', (req, res) => {
+    const queryr = `SELECT NombreProducto, SUM(CantidadProducto) AS TotalVendido
+    FROM Movimiento
+    JOIN Producto_Materia_Prima ON Movimiento.IdProductoMateriaPrima = Producto_Materia_Prima.IdProductoMateriaPrima
+    WHERE TipoMovimiento = 'Salida'
+    GROUP BY NombreProducto
+    ORDER BY TotalVendido ASC;`;
+    conexion.query(queryr, (error, resultado) => {
         if (error) return console.error(error.message);
 
         if (resultado.length > 0) {
